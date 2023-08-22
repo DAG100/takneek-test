253f79f533b2eab8a05fdb47d8fe3abeb532feed
Deven Gangwani <devengangwani12@gmail.com>
2023-08-23 01:57:11 +0530

    Added backend

--- backend/server.mjs
+++ backend/server.mjs
@@ -7,22 +7,19 @@ import mongoose from "mongoose";
 dotenv.config();
 
 const PORT = process.env.PORT || 5001;
+const pass = process.env.PASS || "test";
 const mongoURL = process.env.MONGO_URL || "";
+const event_categories = ["High Prep", "Med Prep", "Low Prep"];
+const pools_names = ["Peshwas", "Aryans", "Nawabs", "Shauryas", "Kshatriyas"]
 
 console.log(`Port: ${PORT}, mongo URL: ${mongoURL}`);
+console.log(pass)
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 
-// const client = new MongoClient(mongoURL);
-// try {
-// 	var conn = await client.connect();
-// } catch (e) {
-// 	console.log("Error occurred when connecting to DB.");
-// 	console.log(e);
-// }
-// leaderboard = conn.db("leaderboard");
-
-mongoose.connect(mongoURL, {});
+await mongoose.connect(mongoURL);
 
 const eventSchema = new mongoose.Schema({
 	//stored event-wise
@@ -32,18 +29,67 @@ const eventSchema = new mongoose.Schema({
 	poolpoints: Array
 });
 
-const eventModel = new mongoose.model("events", eventSchema);
+const eventModel = new mongoose.model("Event", eventSchema);
 
-app.use(cors());
-app.use(express.json());
-
-app.get("/", (request, response) => {
-	//home: send leaderboard
-	response.json({test:"hello"});
-	
+app.get("/", async (request, response) => {
+	//send leaderboard
+	const leaderboard = await eventModel.find({});
+	console.log(leaderboard);
+	response.json(leaderboard);
 });
 
+app.post("/", async (request, response) => {
+	//add an event
+	//first: check password
+	console.log(request);
+	console.log(request.body);
+	if (request.body.password !== pass) {
+		response.json(["Invalid request"]);
+		console.log("Password incorrect");
+		return;
+	}
+	//second: validate request
+	let to_add = {
+		eventname: request.body.eventname,
+		eventcategory: request.body.eventcategory,
+		poolpoints: request.body.poolpoints
+	}
+	//eventcategory: must be member of event_categories
+	if (!event_categories.includes(to_add.eventcategory)) {
+		response.json(["Invalid request"]);
+		console.log("Category not a member of event_categories");
+		return;
+	}
+	//poolpoints: must have all pools as in pools_names
+	//can add points checking here too
+	let check = pools_names;
+	for (const [pool, points] of to_add.poolpoints) {
+		if (check.indexOf(pool) === -1) {
+			response.json(["Invalid request"]);
+			console.log("Found a pool name not in pools_names");
+			return;
+		}
+		check.splice(check.indexOf(pool), 1);
+	}
+	if (check.length > 0) { //not all pools present
+		response.json(["Invalid request"]);
+		console.log("Not all pools included");
+		return;
+	}
+	
+	//finally: add to db
+	to_add = new eventModel(to_add);
+	to_add.save();
+	response.json(["Valid request, saved"]);
+})
 
+app.get("/categories", (req, res) => {
+	res.json(event_categories);
+});
+
+app.get("/pools", (req, res) => {
+	res.json(pools_names);
+});
 
 app.listen(PORT, () => {
 	console.log(`Running on port ${PORT}`);
