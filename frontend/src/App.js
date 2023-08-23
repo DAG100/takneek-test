fd2163d9ae073f4f98dc259761d5310347d957cd
Deven Gangwani <devengangwani12@gmail.com>
2023-08-24 03:17:03 +0530

    Added ability to add events

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,11 +4,26 @@ import Button from "react-bootstrap/Button";
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Form from "react-bootstrap/Form";
+
 import "./App.css";
 
 function App() {
 	const [leaderboard, setLeaderboard] = useState("error");
 	const [refresh, setRefresh] = useState(false); //toggle for refreshing
+	const [formData, setFormData] = useState({
+		password: "",
+		eventname: "",
+		eventcategory: "",
+		poolpoints: {
+			Peshwas: 0,
+			Aryans: 0,
+			Nawabs: 0,
+			Shauryas: 0,
+			Kshatriyas: 0
+		}
+	})
+	const [response, setResponse] = useState("");
 	useEffect(() => {
 		(async () => {
 			//structure of leaderboard at end:
@@ -102,35 +117,55 @@ function App() {
 			</Row>
 		</Container>
 		<Button onClick={() => {setRefresh(!refresh);}}>Refresh</Button>
+		<p>Add event:</p>
+		<Container>
+			<Row>
+				<Col sm={1}><Form.Group>
+					<Form.Label >Event Category</Form.Label>
+					<Form.Select value={formData.eventcategory} onChange={(event) => {setFormData({...formData, eventcategory:event.target.value})}}>
+						<option value="">---</option>
+						{leaderboard.categories.map(el => (
+							<option value={el}>{el}</option>
+						))}
+					</Form.Select>
+				</Form.Group></Col>
+				<Col sm={1}><Form.Group>
+					<Form.Label>Event Name</Form.Label>
+					<Form.Control type="text" placeholder="test" value={formData.eventname} onChange={(event) => {setFormData({...formData, eventname: event.target.value})}}/>
+				</Form.Group></Col>
+				{leaderboard.pools.map(el => (
+				<Col><Form.Group>
+					<Form.Label>{el} Points</Form.Label>
+					<Form.Control type="number" placeholder="" value={formData.poolpoints[el]} onChange={(event) => {
+					let newpoolpoints = formData.poolpoints;
+					newpoolpoints[el] = event.target.value
+					setFormData({
+						...formData, 
+						poolpoints: newpoolpoints
+					})
+					}} />
+				</Form.Group></Col>
+				))}
+			</Row>
+		</Container>
+		<Form.Control type="password" placeholder="Admin Password" value={formData.password} onChange={(event) => {setFormData({...formData, password:event.target.value})}}/>
+		<Button onClick={() => {
+			console.log(formData);
+			setResponse("Waiting...");
+			(async () => {
+				const temp = await (await fetch("/api/", {
+					method: "POST",
+					mode: "cors",
+					headers: {"Content-Type":"application/json"},
+					body: JSON.stringify(formData)
+				})).json();
+				setResponse(temp[0]);
+				setRefresh(!refresh); //refresh leaderboard
+			})();
+		}}>Submit</Button>
+		<p>{response}</p>
 		</div>
 	);
-	
-//   return (
-//     <div className="main">
-//     	{
-//     		leaderboard === "error" 
-//     		? <div className="error">Something went wrong. Check the console for details.</div>
-//     		: <div className="leaderboard">
-//     			{Object.keys(leaderboard.leaderboard).map(el => (
-//     				<div>
-//     					<div>{el}</div>
-//     					<ul>
-//     						{leaderboard.leaderboard[el].map(event=> (
-//     						<li>{event.eventname} - {Object.entries(event.poolpoints).map(el => el.join(" - ")).join(" ")}</li>
-//     						))}
-//     					</ul>
-//     				</div>
-//     			))}
-//     				<div>
-//     					<div>Totals</div>
-//     					<ul><li>
-//     						{Object.entries(leaderboard.total).map(el => el.join(" - ")).join(" ")}
-//     					</li></ul>
-//     				</div>
-//     		</div>
-//     	}
-//     </div>
-//   );
 }
 
 export default App;
