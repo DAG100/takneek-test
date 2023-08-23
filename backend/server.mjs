e362edab8ea4ab3140892fe631e15749a33b9742
Deven Gangwani <devengangwani12@gmail.com>
2023-08-24 02:20:27 +0530

    Frontend data display functionality complete, adding admin access

--- backend/server.mjs
+++ backend/server.mjs
@@ -9,8 +9,6 @@ dotenv.config();
 const PORT = process.env.PORT || 5001;
 const pass = process.env.PASS || "test";
 const mongoURL = process.env.MONGO_URL || "";
-const event_categories = ["High Prep", "Med Prep", "Low Prep"];
-const pools_names = ["Peshwas", "Aryans", "Nawabs", "Shauryas", "Kshatriyas"]
 
 console.log(`Port: ${PORT}, mongo URL: ${mongoURL}`);
 
@@ -20,24 +18,45 @@ app.use(express.json());
 
 await mongoose.connect(mongoURL);
 
+
 const eventSchema = new mongoose.Schema({
 	//stored event-wise
-	//poolpoints: array of arrays [[pool_name, pool_points]]
+	//edit categories, pool names here
 	eventname: String,
-	eventcategory: String,
-	poolpoints: Array
-});
+	eventcategory: {type:String, enum: ["High Prep", "Med Prep", "Low Prep"]},
+	poolpoints: {
+		Peshwas: {type:Number, required: true},
+		Aryans: {type:Number, required: true},
+		Nawabs: {type:Number, required: true},
+		Shauryas: {type:Number, required: true},
+		Kshatriyas: {type:Number, required: true}
+	}
+}, {strict: "throw"});
+
+const event_categories = eventSchema.obj.eventcategory.enum;
+const pools_names = Object.keys(eventSchema.obj.poolpoints);
+
+
+// console.log(eventSchema.obj);
+// console.log(event_categories);
+// console.log(pools_names);
+
 
 const eventModel = new mongoose.model("Event", eventSchema);
 
-app.get("/", async (request, response) => {
+app.get("/", (req, res) => {
+	res.send("Test");
+})
+
+
+app.get("/api/", async (request, response) => {
 	//send leaderboard
 	const leaderboard = await eventModel.find({});
 	console.log(leaderboard);
 	response.json(leaderboard);
 });
 
-app.post("/", async (request, response) => {
+app.post("/api/", async (request, response) => {
 	//add an event
 	//first: check password
 	console.log(request);
@@ -53,42 +72,49 @@ app.post("/", async (request, response) => {
 		eventcategory: request.body.eventcategory,
 		poolpoints: request.body.poolpoints
 	}
-	//eventcategory: must be member of event_categories
-	if (!event_categories.includes(to_add.eventcategory)) {
-		response.json(["Invalid request"]);
-		console.log("Category not a member of event_categories");
-		return;
-	}
-	//poolpoints: must have all pools as in pools_names
-	//can add points checking here too
-	let check = pools_names;
-	for (const [pool, points] of to_add.poolpoints) {
-		if (check.indexOf(pool) === -1) {
-			response.json(["Invalid request"]);
-			console.log("Found a pool name not in pools_names");
-			return;
-		}
-		check.splice(check.indexOf(pool), 1);
-	}
-	if (check.length > 0) { //not all pools present
-		response.json(["Invalid request"]);
-		console.log("Not all pools included");
-		return;
-	}
 	
-	//finally: add to db
-	to_add = new eventModel(to_add);
-	to_add.save();
-	response.json(["Valid request, saved"]);
+// 	//eventcategory: must be member of event_categories
+// 	if (!event_categories.includes(to_add.eventcategory)) {
+// 		response.json(["Invalid request"]);
+// 		console.log("Category not a member of event_categories");
+// 		return;
+// 	}
+// 	//poolpoints: must have all pools as in pools_names
+// 	//can add points checking here too
+// 	let check = pools_names;
+// 	for (const [pool, points] of to_add.poolpoints) {
+// 		if (check.indexOf(pool) === -1) {
+// 			response.json(["Invalid request"]);
+// 			console.log("Found a pool name not in pools_names");
+// 			return;
+// 		}
+// 		check.splice(check.indexOf(pool), 1);
+// 	}
+// 	if (check.length > 0) { //not all pools present
+// 		response.json(["Invalid request"]);
+// 		console.log("Not all pools included");
+// 		return;
+// 	}
+	
+	//schema is set up to validate data -> try turning into model straight away
+	try {
+		to_add = new eventModel(to_add);
+		await to_add.save();
+		response.json(["Valid request, saved"]);
+	} catch (e) {
+		response.json(["Invalid request"]);
+		console.error(e);
+	}
 })
 
-app.get("/categories", (req, res) => {
+app.get("/api/categories", (req, res) => {
 	res.json(event_categories);
 });
 
-app.get("/pools", (req, res) => {
+app.get("/api/pools", (req, res) => {
 	res.json(pools_names);
 });
+
 
 app.listen(PORT, () => {
 	console.log(`Running on port ${PORT}`);
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,136 @@
 import {useState, useEffect} from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from "react-bootstrap/Button";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import "./App.css";
 
 function App() {
-	const [leaderboard, setLeaderboard] = useState([]);
+	const [leaderboard, setLeaderboard] = useState("error");
 	const [refresh, setRefresh] = useState(false); //toggle for refreshing
-	useEffect(async () => {
-		fetch()
+	useEffect(() => {
+		(async () => {
+			//structure of leaderboard at end:
+// 			{
+// 				categories: []
+// 				pools: []
+// 				leaderboard: {
+// 						category_name: [
+// 							{
+// 								eventname, 
+// 								pointspoints: {
+// 								pool: points
+// 							}
+// 						}
+// 					]
+// 				}
+//				total: {pool: points}
+// 			}
+			try {
+				const categories = await(await fetch("/api/categories/")).json();
+				const pools = await (await fetch("/api/pools/")).json();
+				const data = await (await fetch("/api/")).json();
+				let temp_leaderboard = {};
+				let temp_total = {};
+				for (const event_category of categories) {
+					temp_leaderboard[event_category] = [];
+				}
+				for (const pool of pools) {
+					temp_total[pool] = 0;
+				}
+				for (const event of data) {
+					temp_leaderboard[event.eventcategory].push({
+						eventname: event.eventname, 
+						poolpoints: event.poolpoints
+					});
+					for (const pool of pools) {
+						temp_total[pool] += event.poolpoints[pool];
+					}
+				}
+				setLeaderboard({
+					categories: categories,
+					pools: pools,
+					leaderboard: temp_leaderboard,
+					total: temp_total
+				});
+				console.log({
+					categories: categories,
+					pools: await (await fetch("/api/pools/")).json(),
+					leaderboard: temp_leaderboard,
+					total: temp_total
+				});
+			} catch (e) {
+				setLeaderboard("error");
+				console.error(e);
+			}
+		})();
 	}, [refresh]);
 	
-  return (
-    <div className="main">
-    	{
-    		leaderboard === "error" 
-    		? <div className="error">Something went wrong. Try refreshing using the button below.</div>
-    		: <div className="leaderboard">
-    			{leaderboard.map(el => (
-    				<div>{el}</div>
-    			))}
-    		</div>
-    	}
-    </div>
-  );
+	if (leaderboard === "error") return (
+		<div className="error">Someting went wrong. Check the console for details.</div>
+	);
+	else return (
+		<div className="main">
+		<Container>
+			<Row>
+				<Col>Event</Col>
+				{leaderboard.pools.map(el => (
+					<Col>{el}</Col>
+				))}
+			</Row>
+			{leaderboard.categories.map(event_category => (
+				<Row>
+					<Col sm={1}>{event_category}</Col>
+					<Col sm={11} style={{padding: 0}}>
+						<Container style={{width: "100%"}}>
+							{leaderboard.leaderboard[event_category].map(event => (
+								<Row>
+									<Col style={{flexBasis:"9%", flexGrow:"0"}}>{event.eventname}</Col>
+									{Object.entries(event.poolpoints).map(el => (
+											<Col>{el[1]}</Col>
+										))}
+								</Row>
+					))}
+						</Container>
+					</Col>
+				</Row>
+			))}
+			<Row>
+				<Col>Total</Col>
+				{Object.entries(leaderboard.total).map(el => <Col>{el[1]}</Col>)}
+			</Row>
+		</Container>
+		<Button onClick={() => {setRefresh(!refresh);}}>Refresh</Button>
+		</div>
+	);
+	
+//   return (
+//     <div className="main">
+//     	{
+//     		leaderboard === "error" 
+//     		? <div className="error">Something went wrong. Check the console for details.</div>
+//     		: <div className="leaderboard">
+//     			{Object.keys(leaderboard.leaderboard).map(el => (
+//     				<div>
+//     					<div>{el}</div>
+//     					<ul>
+//     						{leaderboard.leaderboard[el].map(event=> (
+//     						<li>{event.eventname} - {Object.entries(event.poolpoints).map(el => el.join(" - ")).join(" ")}</li>
+//     						))}
+//     					</ul>
+//     				</div>
+//     			))}
+//     				<div>
+//     					<div>Totals</div>
+//     					<ul><li>
+//     						{Object.entries(leaderboard.total).map(el => el.join(" - ")).join(" ")}
+//     					</li></ul>
+//     				</div>
+//     		</div>
+//     	}
+//     </div>
+//   );
 }
 
 export default App;
